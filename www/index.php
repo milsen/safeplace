3f4b5b0ef0c078098cc56729b020bce1e86d2cc8
milsen <milsen@users.noreply.github.com>
2015-11-26 14:29:19 +0100

    Improve error-handling in process_file-function
    This index.php-function had return values of different types with some
    errors being stored in errors[] and some being returned. The returned
    errors were then erroneously used as file names.
    Now the function has no return-value anymore. All occurring errors are
    stored in errors[] with four keys:
        'number'  => error level
        'message' => description of error
        'file'    => file in which error occurred
        'line'    => line in which error occurred
    
    This display of the errors in single.phtml was changed such that the
    file name and line number are actually shown. Before, the
    title-attribute was used although the li-tag does not have such an
    attribute.

--- www/index.php
+++ www/index.php
@@ -26,27 +26,59 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 	exit;
 }
 
-function process_file($file, array &$errors = array())
+/**
+ * Check uploaded file for various errors (wrong filename, upload error, wrong
+ * knowledge base syntax, error while moving file on server) and describe each
+ * error in $errors using the keys 'number', 'message', 'file' and 'line'.
+ * If no errors occurr, the uploaded file is moved to MODIFIED_KB.
+ *
+ * @param array $kb_file Knowledge base file that is uploaded.
+ * @param array &$errors Filled with a description of all errors:
+ *		'number'  => error level
+ *		'message' => description of error
+ *		'file'    => file in which error occurred
+ *		'line'    => line in which error occurred
+ * @return void
+ */
+function process_file($kb_file, array &$errors = array())
 {
-	if ($file['error'] != 0)
-		return "Er is een fout opgetreden bij het uploaden.";
+	// create default values of error level, filename and line number
+	// to describe errors which occurr while uploading and not while linting
+	$number = E_USER_WARNING;
+	$file = "Bestand te uploaden";
+	$line = 0;
+	$number_file_line = array("number","file","line");
 
+	// check filename
+	if (!preg_match('/^[a-zA-Z0-9_\-\.]+\.xml$/i', $kb_file['name'])) {
+		$message = "De bestandsnaam bevat karakters die niet goed " .
+			"verwerkt kunnen worden. Het bestand moet zijn een " .
+			"XML-bestand.";
+		$errors[] = (object) compact("message",$number_file_line);
+		return;
+	}
+
+	// check for upload errors: file size too large, connection error etc.
+	if ($kb_file['error'] != 0) {
+		$message = "Er is een fout opgetreden bij het uploaden.";
+		$errors[] = (object) compact("message",$number_file_line);
+		return;
+	}
+
+	// check knowledge base syntax
 	$reader = new KnowledgeBaseReader();
-	$errors = $reader->lint($file['tmp_name']);
+	$errors = $reader->lint($kb_file['tmp_name']);
 
-	if (count($errors) > 0)
-	{
-		return false;
+	if (count($errors) > 0) {
+		return;
 	}
 
 	// save uploaded file in MODIFIED_KB, overwrite it if it already exists
-	if (!move_uploaded_file($file['tmp_name'], MODIFIED_KB))
-	{
-		$errors[] = "De knowledge-base kon niet worden opgeslagen op de server.";
-		return false;
+	if (!move_uploaded_file($kb_file['tmp_name'], MODIFIED_KB)) {
+		$message = "De knowledge-base kon niet worden opgeslagen op de " .
+			"server.";
+		$errors[] = (object) compact("message",$number_file_line);
 	}
-
-    return true;
 }
 
 $template = new Template('templates/single.phtml');
